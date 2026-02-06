f557953ab34851dc9911a335573cf4ccc4618339
Encore Team <team@encorewood.com>
2026-02-06 19:14:16 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -56,6 +56,7 @@ import ShopProduction from './pages/ShopProduction';
 import Team from './pages/Team';
 import ChatBoard from './pages/ChatBoard';
 import Inventory from './pages/Inventory';
+import BoardSettings from './pages/BoardSettings';
 import __Layout from './Layout.jsx';
 
 
@@ -69,6 +70,7 @@ export const PAGES = {
     "Team": Team,
     "ChatBoard": ChatBoard,
     "Inventory": Inventory,
+    "BoardSettings": BoardSettings,
 }
 
 export const pagesConfig = {
